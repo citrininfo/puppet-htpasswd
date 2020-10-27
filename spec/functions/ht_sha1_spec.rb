7515dec83636211ff31ec92297e1eb2be5822283
Patrick Kiefer <patrick.kiefer@citrin.ch>
2020-10-27 12:32:42 +0100

    update Testing to rspec-puppetv2.7.x

--- spec/functions/ht_sha1_spec.rb
+++ spec/functions/ht_sha1_spec.rb
@@ -1,26 +1,20 @@
 require 'spec_helper'
 
-describe "the ht_sha1 function" do
+describe "htpasswd::ht_sha1" do
   let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
-  it "should exist" do
-    Puppet::Parser::Functions.function("ht_sha1").should == "function_ht_sha1"
+  it { is_expected.to run.with_params('password') }
+
+  it { is_expected.to run.with_params([]).and_raise_error(ArgumentError) }
+
+  it { is_expected.to run.with_params('to','many','arguments').and_raise_error(ArgumentError) }
+
+  it "should raise a ArgumentError if passed not a string" do
+    is_expected.to run.with_params(24).and_raise_error(ArgumentError)
   end
 
-  it "should raise a ParseError if there is less than 1 argument" do
-    lambda { scope.function_ht_sha1([]) }.should( raise_error(Puppet::ParseError))
+  it "should return a MD5 password" do
+    is_expected.to run.with_params('testpassword').and_return('{SHA}i7YRj4/Wk1rQh2o740pxfTJwj/0=')
   end
 
-  it "should raise a ParseError if there is more than 1 argument" do
-    lambda { scope.function_ht_sha1(['foo', 'bar']) }.should( raise_error(Puppet::ParseError))
-  end
-
-  it "should raise a ParseError if passed not a string" do
-    lambda { scope.function_ht_sha1([42]) }.should( raise_error(Puppet::ParseError))
-  end
-
-  it "should return a SHA1 password" do
-    result = scope.function_ht_sha1(['testpassword'])
-    result.should(eq('{SHA}i7YRj4/Wk1rQh2o740pxfTJwj/0='))
-  end
 end
