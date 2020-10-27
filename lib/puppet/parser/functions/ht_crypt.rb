fef90b695c9ed072f0a3ad6a584a3e25b2b36274
Patrick Kiefer <patrick.kiefer@citrin.ch>
2020-10-27 11:24:56 +0100

    rewrite crypt function to 4.x API

--- lib/puppet/parser/functions/ht_crypt.rb
+++ lib/puppet/parser/functions/ht_crypt.rb
@@ -1,17 +1,12 @@
-module Puppet::Parser::Functions
-    newfunction(:ht_crypt, :type => :rvalue, :doc => <<-EOS
-      encrypt a password using crypt. The first argument is the password and the second one the salt to use
-    EOS
-    ) do |args|
-      raise(Puppet::ParseError, "ht_crypt(): Wrong number of arguments " +
-        "given (#{args.size} for 2)") if args.size != 2
+# Encrypt Password with crypt
+Puppet::Functions.create_function(:'htpasswd::ht_crypt') do
+  dispatch :crypt do
+    param 'String', :password
+    param 'String', :salt
+    return_type 'String'
+  end
 
-      value = args[0]
-      salt = args[1]
-
-      raise(Puppet::ParseError, 'ht_crypt(): Requires a string to work with') unless value.class == String
-      raise(Puppet::ParseError, 'ht_crypt(): Requires a string to work with') unless salt.class == String
-
-      value.crypt(salt)
-    end
+  def crypt(password, salt)
+    password.crypt(salt)
+  end
 end
