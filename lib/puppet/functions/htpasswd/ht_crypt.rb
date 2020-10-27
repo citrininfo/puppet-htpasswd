02f30ca74593c35231d776858f03255bba9b6b17
Patrick Kiefer <patrick.kiefer@citrin.ch>
2020-10-27 11:54:28 +0100

    fix names to expectation

--- lib/puppet/functions/htpasswd/ht_crypt.rb
+++ lib/puppet/functions/htpasswd/ht_crypt.rb
@@ -1,12 +1,12 @@
 # Encrypt Password with crypt
 Puppet::Functions.create_function(:'htpasswd::ht_crypt') do
-  dispatch :crypt do
+  dispatch :ht_crypt do
     param 'String', :password
     param 'String', :salt
     return_type 'String'
   end
 
-  def crypt(password, salt)
+  def ht_crypt(password, salt)
     password.crypt(salt)
   end
 end
