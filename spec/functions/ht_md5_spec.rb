099c1cf901e20ac0e3aa20bf5b4bf6f75c880d09
Patrick Kiefer <patrick.kiefer@citrin.ch>
2020-10-27 12:30:12 +0100

    update Testing to rspec-puppetv2.7.x

--- spec/functions/ht_md5_spec.rb
+++ spec/functions/ht_md5_spec.rb
@@ -1,26 +1,19 @@
 require 'spec_helper'
 
-describe "the ht_md5 function" do
+describe "htpasswd::ht_md5" do
   let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
 
-  it "should exist" do
-    Puppet::Parser::Functions.function("ht_md5").should == "function_ht_md5"
-  end
+  it { is_expected.to run.with_params('password', 'salt') }
 
-  it "should raise a ParseError if there is less than 2 argument" do
-    lambda { scope.function_ht_md5(['test']) }.should( raise_error(Puppet::ParseError))
-  end
+  it { is_expected.to run.with_params('missingArgument').and_raise_error(ArgumentError) }
 
-  it "should raise a ParseError if there is more than 2 arguments" do
-    lambda { scope.function_ht_md5(['foo', 'bar', 'ff']) }.should( raise_error(Puppet::ParseError))
-  end
+  it { is_expected.to run.with_params('to','many','arguments').and_raise_error(ArgumentError) }
 
-  it "should raise a ParseError if passed not a string" do
-    lambda { scope.function_ht_md5([42, 'str']) }.should( raise_error(Puppet::ParseError))
+  it "should raise a ArgumentError if passed not a string" do
+    is_expected.to run.with_params(24,'wrong_type').and_raise_error(ArgumentError)
   end
 
   it "should return a MD5 password" do
-    result = scope.function_ht_md5(['testpassword', 'PhT5FuSg'])
-    result.should(eq('$apr1$PhT5FuSg$3o4QbIJfx4SZMLaa9T1A9.'))
+    is_expected.to run.with_params('testpassword', 'PhT5FuSg').and_return('$apr1$PhT5FuSg$3o4QbIJfx4SZMLaa9T1A9.')
   end
 end
